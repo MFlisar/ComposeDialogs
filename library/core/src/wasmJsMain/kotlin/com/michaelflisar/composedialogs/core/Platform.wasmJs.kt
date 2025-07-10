d68417d06f76f57c806d48fc916ceb0244f552af
MFlisar <mflisar.development@gmail.com>
2025-07-10 12:45:49 +0200

    ...

---
+++ library/core/src/wasmJsMain/kotlin/com/michaelflisar/composedialogs/core/Platform.wasmJs.kt
@@ -0,0 +1,70 @@
+package com.michaelflisar.composedialogs.core
+
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+actual fun isLandscape(): Boolean = false
+
+@Composable
+actual fun stringOk(): String = "OK"
+
+@Composable
+actual fun DialogDefaults.defaultDialogStyle(): ComposeDialogStyle = TODO()
+
+@Composable
+actual fun DialogContentScrollableColumn(
+    modifier: Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = modifier
+            .verticalScroll(scrollState)
+    ) {
+        content()
+    }
+}
+
+@Composable
+actual fun DialogContentScrollableLazyColumn(
+    modifier: Modifier,
+    state: LazyListState,
+    contentPadding: PaddingValues,
+    reverseLayout: Boolean,
+    verticalArrangement: Arrangement.Vertical,
+    horizontalAlignment: Alignment.Horizontal,
+    flingBehavior: FlingBehavior,
+    userScrollEnabled: Boolean,
+    content: LazyListScope.() -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+            .padding(end = 4.dp),
+        state = state,
+        contentPadding = contentPadding,
+        reverseLayout = reverseLayout,
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment,
+        flingBehavior = flingBehavior,
+        userScrollEnabled = userScrollEnabled
+    ) {
+        content()
+    }
+}
+
+@Composable
+actual fun updateStatusbarColor(darkStatusBar: Boolean) {}
